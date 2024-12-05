20d83df4848e71f554e7a55ece1d49a900dd2310
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-12-05 16:42:46 +0900

    create project

---
+++ src/test/java/imgsystem/demo/ImgSystemDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package imgsystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgSystemDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
